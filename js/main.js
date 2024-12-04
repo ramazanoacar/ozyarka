427c1d75e4763a6c949c5e73074a7b25ccacf991
ramazanoacar <ramazan.acar@boun.edu.tr>
2024-12-05 02:55:17 +0300

    add new theme

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// main.js
-
-// İletişim Formu Doğrulama
-document.querySelector('form').addEventListener('submit', function(e) {
-    // Form doğrulama işlemleri burada yapılabilir
-    // Örneğin, CAPTCHA ekleme veya veri doğrulama
-    // Şu anda örnek olarak sadece uyarı veriliyor
-    alert('Mesajınız gönderildi!');
-});
